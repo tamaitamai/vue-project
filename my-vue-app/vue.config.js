d11b423f006b9ee7032b41f1ae61bb8d04235882
tamaitamai <yktmi0217@gmail.com>
2025-04-08 09:54:37 +0900

    vue.config.jsのほうも修正

--- my-vue-app/vue.config.js
+++ my-vue-app/vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        // target: 'http://localhost:8080',
+        target: 'https://vue-backend-project.onrender.com',
         changeOrigin: true,
         pathRewrite: { '^/api': '' },
       },
