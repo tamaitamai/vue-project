1b3a16ecde437529dd051fb2320567c9b74c5c43
tamaitamai <yktmi0217@gmail.com>
2025-04-08 11:47:06 +0900

    WebConfig.javaの修正

--- src/main/java/com/example/WebConfig.java
+++ src/main/java/com/example/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        // .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("https://vue-project-vw4z.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
